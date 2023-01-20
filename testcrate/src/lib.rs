68a9a0eae7968094077b9690c05ad540da1adc73
zhangjingqiang <zhangjingqiang@bytedance.com>
2023-01-20 11:39:02 +0800

    initial commit with files from openssl-src-rs

---
+++ testcrate/src/lib.rs
@@ -0,0 +1,15 @@
+extern crate libc;
+
+use libc::c_ulong;
+
+extern "C" {
+    pub fn OpenSSL_version_num() -> c_ulong;
+}
+
+#[test]
+fn version_works() {
+    unsafe {
+        println!("{:#x}", OpenSSL_version_num());
+        assert!(OpenSSL_version_num() > 0);
+    }
+}
