6301af23dbbea5e9d47d9e35091578b597b4172b
Zhang Jingqiang <zh_jq@outlook.com>
2023-12-25 11:03:10 +0800

    try to fix build for windows

--- src/lib.rs
+++ src/lib.rs
@@ -665,8 +665,8 @@ impl Artifacts {
         }
         println!("cargo:include={}", self.include_dir.display());
         println!("cargo:lib={}", self.lib_dir.display());
-        if self.target.contains("windows") {
-            println!("cargo:rustc-link-lib=crypt32");
+        if self.target.contains("msvc") {
+            println!("cargo:rustc-link-lib=user32");
         } else if self.target == "wasm32-wasi" {
             println!("cargo:rustc-link-lib=wasi-emulated-signal");
             println!("cargo:rustc-link-lib=wasi-emulated-process-clocks");
